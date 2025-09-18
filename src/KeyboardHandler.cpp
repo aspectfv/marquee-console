c764cb49964340e6f87c1d08a8805555ef13a172
aspectzxcc <josh10nathan@gmail.com>
2025-09-19 01:16:59 +0800

    updated keyboard handler

--- src/KeyboardHandler.cpp
+++ src/KeyboardHandler.cpp
@@ -5,9 +5,8 @@
 KeyboardHandler::KeyboardHandler(SystemContext& context) : system_context_ref(context) {}
 
 void KeyboardHandler::run() {
-    while (system_context_ref.is_running) {
-        std::string input;
-        std::getline(std::cin, input);
+    std::string input;
+    while (system_context_ref.is_running && std::getline(std::cin, input)) {
         if (!input.empty()) system_context_ref.command_queue.push(input);
     }
 }
