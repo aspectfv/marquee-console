539e7d2b710d9f00a675c1366528ebadae267436
aspectzxcc <josh10nathan@gmail.com>
2025-09-19 03:31:47 +0800

    applied command pattern to command interpreter

--- include/CommandInterpreter.hpp
+++ include/CommandInterpreter.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include "SystemContext.hpp"
+#include <unordered_map>
+#include <functional>
+#include <sstream>
 
 class CommandInterpreter {
     SystemContext& system_context_ref;
+    std::unordered_map<std::string, std::function<std::string(std::stringstream&)>> commands;
+
+    void initialize_commands();
 
 public:
     CommandInterpreter(SystemContext& context);
--- src/CommandInterpreter.cpp
+++ src/CommandInterpreter.cpp
@@ -1,53 +1,72 @@
-#include "CommandInterpreter.hpp"
-#include <sstream>
+// src/CommandInterpreter.cpp
 
-CommandInterpreter::CommandInterpreter(SystemContext& context) : system_context_ref(context) {}
+#include "CommandInterpreter.hpp"
+
+CommandInterpreter::CommandInterpreter(SystemContext& context) : system_context_ref(context) {
+    initialize_commands();
+}
 
 void CommandInterpreter::process_next_command() {
     std::optional<std::string> command_line_opt = system_context_ref.command_queue.pop();
     if (!command_line_opt) return;
 
-    std::string command_line = *command_line_opt;
-    std::stringstream ss(command_line);
+    std::stringstream ss(*command_line_opt);
     std::string command;
     ss >> command;
 
-    std::string msg;
-    bool command_processed = true;
+    if (command.empty()) return;
 
-    if (command == "exit") {
-        msg = "Exiting application...";
-        system_context_ref.is_running = false;
-    } else if (command == "help") {
-        msg = "Commands: help, exit, start_marquee, stop_marquee, set_text <text>, set_speed <ms>";
-    } else if (command == "start_marquee") {
-        system_context_ref.marquee_state.set_active(true);
-        command_processed = true;
-    } else if (command == "stop_marquee") {
-        system_context_ref.marquee_state.set_active(false);
-        msg = "Marquee stopped.";
-    } else if (command == "set_text") {
-        std::string text;
-        std::getline(ss, text);
-        if (!text.empty() && text[0] == ' ') text.erase(0, 1); // remove leading space
-        system_context_ref.marquee_state.set_text(text);
-        msg = "Marquee text set to '" + text + "'.";
-    } else if (command == "set_speed") {
-        int speed;
-        if (ss >> speed && speed > 0) {
-            system_context_ref.marquee_state.set_text_ms(speed);
-            msg = "Marquee speed set to " + std::to_string(speed) + " ms.";
-        } else {
-            msg = "Invalid speed value.";
-        }
-    } else if (!command.empty()) {
-        msg = "Unknown command: '" + command + "'. Type 'help' for a list of commands.";
+    std::string msg;
+    auto it = commands.find(command);
+    if (it != commands.end()) {
+        msg = it->second(ss);
     } else {
-        command_processed = false;
+        msg = "Unknown command: '" + command + "'. Type 'help' for a list of commands.";
     }
-    
-    if (command_processed) {
+
+    if (!msg.empty()) {
         std::lock_guard<std::mutex> lock(system_context_ref.prompt_mutex);
         system_context_ref.prompt_display_buffer = msg;
     }
+}
+
+void CommandInterpreter::initialize_commands() {
+    commands["help"] = [](std::stringstream&) -> std::string {
+        return "Commands: help, exit, start_marquee, stop_marquee, set_text <text>, set_speed <ms>";
+    };
+
+    commands["exit"] = [this](std::stringstream&) -> std::string {
+        system_context_ref.is_running = false;
+        return "Exiting application...";
+    };
+
+    commands["start_marquee"] = [this](std::stringstream&) -> std::string {
+        system_context_ref.marquee_state.set_active(true);
+        return "Marquee started.";
+    };
+
+    commands["stop_marquee"] = [this](std::stringstream&) -> std::string {
+        system_context_ref.marquee_state.set_active(false);
+        return "Marquee stopped.";
+    };
+
+    commands["set_text"] = [this](std::stringstream& ss) -> std::string {
+        std::string text;
+        std::getline(ss, text);
+        if (!text.empty() && text[0] == ' ') text.erase(0, 1); // remove leading space
+        
+        if (text.empty()) return "Error: No text provided for set_text.";
+
+        system_context_ref.marquee_state.set_text(text);
+        return "Marquee text set to '" + text + "'.";
+    };
+
+    commands["set_speed"] = [this](std::stringstream& ss) -> std::string {
+        int speed;
+        if (ss >> speed && speed > 0) {
+            system_context_ref.marquee_state.set_text_ms(speed);
+            return "Marquee speed set to " + std::to_string(speed) + " ms.";
+        }
+        return "Invalid speed value. Please provide a positive number.";
+    };
 }
