2d9ff06a8e0d60619edbc9e42b2496cba9767b3b
aspectzxcc <josh10nathan@gmail.com>
2025-09-29 21:51:01 +0800

    added logic to always display asking for command output to below marquee animation

--- src/DisplayHandler.cpp
+++ src/DisplayHandler.cpp
@@ -6,14 +6,21 @@ DisplayHandler::DisplayHandler(SystemContext& context, MarqueeLogic& logic) : sy
 
 void DisplayHandler::run() {
     std::cout << "\033[2J\033[H" << "Command > " << std::flush; // clear screen and move cursor top left
+    int curr_line = 1;
 
     while (system_context_ref.is_running) {
         if (system_context_ref.marquee_state.get_active()) {
-            std::cout << "\033[s"      // save cursor pos
-                      << "\033[1;1H"   // move cursor top left
+            if (curr_line < 11) {
+                int diff = 11 - curr_line;
+                std::cout << "\033[" << diff << "B"; // move cursor below marquee area
+                curr_line = 11;
+            }
+
+            std::cout << "\033[s" // save cursor pos
+                      << "\033[1;1H" // move cursor top left
                       << marquee_logic_ref.get_next_frame() // draw marquee
-                      << "\033[K"      // clear to end of line
-                      << "\033[u"      // restore cursor to user input line
+                      << "\033[K" // clear to end of line
+                      << "\033[u" // restore cursor to user input line
                       << std::flush;
         }
 
@@ -26,7 +33,10 @@ void DisplayHandler::run() {
             }
         }
 
-        if (!msg.empty()) std::cout << "\n" << msg << "\nCommand > " << std::flush;
+        if (!msg.empty()) {
+          std::cout << "\n" << msg << "\nCommand > " << std::flush;
+          curr_line += 2;
+        }
 
         int speed = system_context_ref.marquee_state.get_text_ms();
         std::this_thread::sleep_for(std::chrono::milliseconds(speed));
